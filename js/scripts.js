324ccfb6f432d82dd2745ab37accd031157df855
Arthur Couto <70111948+ArthurFCouto@users.noreply.github.com>
2022-08-01 15:10:15 -0300

    Atualização no script

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,16 @@
-const alterAlert = (nameClass, text, method) => {
+const alterAlert = (nameClass, text) => {
     const alert = document.getElementById('alert');
+    alert.classList.remove('alert-primary');
+    alert.classList.remove('alert-danger');
+    alert.classList.remove('alert');
     alert.classList.add('alert');
-    if (method === 'add') {
-        alert.classList.add(nameClass);
-    } else {
-        alert.classList.remove(nameClass);
-    }
+    alert.classList.add(nameClass);
     alert.innerHTML = text;
 }
 
 const handleErrorArray = (errorList) => {
     errorList.forEach((error) => {
-        const field = error.field + "Help";
+        const field = error.field + 'Help';
         const help = document.getElementById(field);
         if (help)
             help.innerHTML = error.error;
@@ -20,21 +19,22 @@ const handleErrorArray = (errorList) => {
 
 function newUser(event) {
     const inputs = document.querySelectorAll('[data-input-edit="input"]');
-    [...inputs].map(element => element.setAttribute("disabled", true));
+    [...inputs].map(element => element.setAttribute('disabled', true));
     const helps = document.querySelectorAll('[data-input-edit="help"]');
-    [...helps].map(element => element.innerHTML = "");
+    [...helps].map(element => element.innerHTML = '');
     const nome = event.target[0].value;
     const sobrenome = event.target[1].value;
     const body = {
-        nome: nome.concat(" ", sobrenome),
+        nome: nome.concat(' ', sobrenome),
         escola: event.target[2].value,
         turma: event.target[3].value,
         email: event.target[4].value,
         telefone: event.target[5].value,
-        senha: "12345678"
+        senha: '12345678'
     };
-    alterAlert('alert-primary', 'Aguarde, cadastrando...', 'add');
+    alterAlert('alert-primary', 'Aguarde, cadastrando...');
     fetch('https://bsi-cadastro-usuarios.herokuapp.com/user', {
+        mode:'no-cors',
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
@@ -44,18 +44,20 @@ function newUser(event) {
         .then(async (response) => {
             const responseJson = await response.json();
             if (response.status === 200) {
-                alterAlert('alert-primary', 'Cadastro efetuado! Obrigado por participar!', 'add');
+                alterAlert('alert-primary', 'Cadastro efetuado! Obrigado por participar!');
                 setTimeout(() => window.history.back(), 1500);
             } else {
-                [...inputs].map(element => element.removeAttribute("disabled"));
-                alterAlert('alert-primary', 'Ops tivemos um erro...', 'remove');
-                alterAlert('alert-danger', 'Houve um erro durante o cadastro, confira os campos digitados!', 'add');
+                [...inputs].map(element => element.removeAttribute('disabled'));
                 const { error } = responseJson;
                 if (Array.isArray(error)) {
+                    alterAlert('alert-danger', 'Houve um erro durante o cadastro, confira os campos digitados');
                     handleErrorArray(error);
                 } else {
-                    alterAlert('alert-danger', error, 'add');
+                    alterAlert('alert-danger', error);
                 }
             }
+        })
+        .catch((error)=> {
+            alterAlert('alert-danger', 'Ops! Tivemos um erro, tente novamente mais tarde');
         })
 }
